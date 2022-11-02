dba69df07b1ab67454be8333dff6464143d614e6
Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
2022-11-01 17:17:57 -0700

    gallery links added

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -36,14 +36,26 @@ export function GalleryItem({ track }) {
     </div>
   )
 
-  const detailedView = (
-    <div style={detailStyle} >
-      <h2>{track.trackName}</h2>
-      <h3>{track.collectionName}</h3>
-      <h4>{track.primaryGenreName}</h4>
-      <h4>{track.releaseDate}</h4>
-    </div>
-  )
+  const detailView = () => {
+    return (
+        <div style={detailStyle}>
+            <h2>{props.item.trackName}</h2>
+            <h3>
+                <a href={`/artist/${props.item.artistId}`}>
+                    {props.item.artistName}
+                </a>
+            </h3>
+            <h3>
+                <a href={`/album/${props.item.collectionId}`}>
+                    {props.item.collectionName}
+                </a>
+            </h3>
+            <h4>{props.item.primaryGenreName}</h4>
+            <h4>{props.item.releaseDate}</h4>
+        </div>
+    )
+}
+
 
 
 
